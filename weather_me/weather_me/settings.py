f8951c6727a12a56ea6cf27e5b62eff45a03a9d8
Ray-Not <2sinsincuba@gmail.com>
2024-07-17 14:24:33 +0500

    Add auto redirect

--- weather_me/weather_me/settings.py
+++ weather_me/weather_me/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'get_weather.middleware.RedirectToMainURLMiddleware',
 ]
 
 ROOT_URLCONF = 'weather_me.urls'
