452e42ac46309a075c58b6a718be150872dfab3c
Ray-Not <2sinsincuba@gmail.com>
2024-07-17 13:10:21 +0500

    Add form

--- weather_me/get_weather/views.py
+++ weather_me/get_weather/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from .utils import get_weather
+from .forms import CityForm
 
 
 def index_view(request):
-    context = get_weather('Moscow')
-    # context = {}
+    context = {}
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        if form.is_valid():
+            city = form.cleaned_data['city']
+            context = get_weather(city)
+    else:
+        form = CityForm()
+    context['form'] = form
     return render(request, 'index.html', context)
