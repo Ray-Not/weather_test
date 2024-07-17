63bd0af1176445d5e8e39a6f2e1e33e6d93405e3
Ray-Not <2sinsincuba@gmail.com>
2024-07-17 13:23:34 +0500

    Preview functional

--- weather_me/get_weather/views.py
+++ weather_me/get_weather/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from .utils import get_weather
 from .forms import CityForm
+# from django.contrib.auth.decorators import login_required
 
 
+# @login_required
 def index_view(request):
     context = {}
     if request.method == 'POST':
         form = CityForm(request.POST)
         if form.is_valid():
             city = form.cleaned_data['city']
+            # SearchHistory.objects.create(user=request.user, city=city)
             context = get_weather(city)
     else:
         form = CityForm()
